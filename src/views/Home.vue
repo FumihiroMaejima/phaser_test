37fb494baa015f77e280c54e59f3e789afd81631
Fumihiro_Maejima <testerwatson1101@gmail.com>
2020-11-21 23:24:14 +0900

    modify home component.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,20 @@
 <template>
   <div class="container mx-auto">
-    <div class="home">
-      <img alt="Vue logo" src="../assets/logo.png" />
-      <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <div class="bg-gray-500 my-1 sm:mx-1">
+      test
+      <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" /> -->
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
 
 export default defineComponent({
   name: 'Home',
   components: {
-    HelloWorld
+    // HelloWorld
   }
 })
 </script>

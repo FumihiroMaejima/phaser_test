a6db469abf5e29d24aa7ba9e9f9b91ead3b2d466
Fumihiro_Maejima <testerwatson1101@gmail.com>
2020-12-18 01:25:15 +0900

    add input component.

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -235,6 +235,15 @@
       <chip />
     </grid-cols>
 
+    <h1 class="italic my-2">component input test</h1>
+    <grid-cols option="bg-purple-100 py-2 px-2">
+      <app-input
+        placeholder="test@example.com"
+        @app-input="catchAppInputEvent"
+      />
+      <chip />
+    </grid-cols>
+
     <div class="mt-10 grid rounded md:grid-cols-3 sm:grid-cols-1 gap-4"></div>
   </div>
 </template>
@@ -242,6 +251,7 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 import AppButton from '@/components/parts/AppButton.vue'
+import AppInput from '@/components/parts/AppInput.vue'
 import Chip from '@/components/parts/Chip.vue'
 import GridCols from '@/components/parts/GridCols.vue'
 import GridRows from '@/components/parts/GridRows.vue'
@@ -250,12 +260,24 @@ export default defineComponent({
   name: 'Home',
   components: {
     AppButton,
+    AppInput,
     Chip,
     GridCols,
     GridRows
   },
   setup() {
-    return {}
+
+    // methods
+    /**
+     * catch app-input event
+     * @return {void}
+     */
+    const catchAppInputEvent = (event: any) => {
+      console.log('catchAppInputEvent: ' + JSON.stringify(event, null, 2))
+    }
+    return {
+      catchAppInputEvent
+    }
   }
 })
 </script>

791f49276db4df951171b24b74a8def4e949a9c3
Fumihiro_Maejima <testerwatson1101@gmail.com>
2021-05-30 23:30:47 +0900

    add background image.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -14,10 +14,24 @@ import GlobalFooter from '@/components/_global/GlobalFooter.vue'
 export default defineComponent({
   components: {
     GlobalHeader,
-    GlobalFooter
+    GlobalFooter,
   },
   setup() {
     return {}
-  }
+  },
 })
 </script>
+<style lang="scss">
+body {
+  // background: url('./assets/img/testGrid.svg') repeat;
+  background-image: url('./assets/img/testBackGround.svg');
+  background-repeat: no-repeat;
+  background-position: center;
+  // 画面をスクロールする際、 背景画像をその位置に固定
+  background-attachment: fixed;
+  // 画像の縦横比を崩すことなく画像ができるだけ大きくなるよう拡大縮小させる
+  background-size: cover;
+  // 背景画像が読み込まれる前に表示される背景のカラー
+  background-color: #002b40;
+}
+</style>
